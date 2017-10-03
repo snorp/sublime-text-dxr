52684f3f4db3a6d61d56bb50016d176eaa934ff1
James Willcox <snorp@snorp.net>
2017-10-03 13:17:54 -0500

    Convert indentation to spaces because I'm not a savage

--- dxr.py
+++ dxr.py
@@ -6,26 +6,26 @@ from os import path
 DXR_URL = "https://dxr.mozilla.org/mozilla-central/source"
 
 class OpenInDxrCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		def find_sourcedir(file_name):
-			directory = path.dirname(file_name)
-			if directory == file_name:
-				return None
+    def run(self, edit):
+        def find_sourcedir(file_name):
+            directory = path.dirname(file_name)
+            if directory == file_name:
+                return None
 
-			mach_path = path.join(directory, 'mach')
-			if path.exists(mach_path):
-				return directory
+            mach_path = path.join(directory, 'mach')
+            if path.exists(mach_path):
+                return directory
 
-			return find_sourcedir(directory)
+            return find_sourcedir(directory)
 
-		# self.view.insert(edit, 0, "DXR!")
-		file_name = self.view.file_name()
-		source_dir = find_sourcedir(file_name)
-		if not source_dir:
-			return
+        # self.view.insert(edit, 0, "DXR!")
+        file_name = self.view.file_name()
+        source_dir = find_sourcedir(file_name)
+        if not source_dir:
+            return
 
-		relative_name = self.view.file_name()[len(source_dir):]
-		line_number = self.view.rowcol(self.view.sel()[0].begin())[0] + 1
+        relative_name = self.view.file_name()[len(source_dir):]
+        line_number = self.view.rowcol(self.view.sel()[0].begin())[0] + 1
 
-		url = "%s%s#%d" % (DXR_URL, relative_name, line_number)
-		webbrowser.open_new_tab(url)
+        url = "%s%s#%d" % (DXR_URL, relative_name, line_number)
+        webbrowser.open_new_tab(url)
